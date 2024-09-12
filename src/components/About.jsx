1c4f5cb35f1d934a6ed65c7a6def069d97fa05a4
RGCENA <raviganesh387@gmail.com>
2024-09-12 22:48:10 +0530

    demofolio

---
+++ src/components/About.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const About = () => {
+    return(
+      <div className="border-b border-neutral-900 pb-400">
+      <h2 className="my-20 text-center text-4xl">About
+        <span className="text-neutral-500"> Me</span>
+
+      </h2>
+      </div>
+    )
+} 
+
+export default About;
